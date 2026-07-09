3aaf585d671a1e675d5a5ecb71951d38abdb5150
Arash-lab-cell <singharashpreet17@gmail.com>
2026-07-09 19:58:44 +0530

    Implement solution to set matrix rows and columns to zero

---
+++ 2D Matrices/Row to Column Zero/Solution.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public int[][] solve(int[][] A) {
+        int n = A.length;
+        int m = A[0].length;
+        boolean[] row = new boolean[n];
+        boolean[] col = new boolean[m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (A[i][j] == 0) {
+                    row[i] = true;
+                    col[j] = true;
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (row[i] || col[j]) {
+                    A[i][j] = 0;
+                }
+            }
+        }
+        return A;
+    }
+}
